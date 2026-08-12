f706d56ea36ae54d216ea8028a66e631715f440a
ybtamiru <ybtamiruasefa@gmail.com>
2026-08-12 09:41:27 +0300

    before production test

--- Front-End/src/environments/environment.ts
+++ Front-End/src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:5011/api'
-  // apiUrl: 'http://192.168.1.4:5011/api'
+  // apiUrl: 'http://localhost:5011/api'
+  apiUrl: 'http://192.168.1.4:5011/api'
 };
